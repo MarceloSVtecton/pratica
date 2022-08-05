63b61765df41aebc21f39dd5b16c48867736522e
Marcelo de Souza Vicente <mdesouzavicente475@gmail.com>
2022-08-05 12:21:53 -0300

    alteraçoes css

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 function carregar() {
-    var msg = document.getElementById('msg');
-    var fotos = document.getElementById('fotos');
-    var saud = document.getElementById('saudaçao');
-    var data = new Date();
-    var hora = data.getHours();
-    var min = data.getMinutes();
-    var horario = 'AGORA SÃO '+ hora + ' HORAS '+ min + ' MINUTOS.';
-    msg.innerText = `AGORA SÃO ${horario} .`;
+    var msg = document.getElementById('msg')
+    var fotos = document.getElementById('fotos')
+    var saud = document.getElementById('saudaçao')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    var horario = 'AGORA SÃO '+ hora + ' HORAS '+ min + ' MINUTOS.'
+    msg.innerText = `AGORA SÃO ${horario} .`
     if(hora >=0 && hora <12) {
         fotos.src = 'dia.png'
         saud.innerHTML = 'BOM DIA'
